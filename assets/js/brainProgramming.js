60ac208d98c1327ff393e3eb3f70d10f0c02599f
Jackie Stonebraker <sunktanka@gmail.com>
2017-01-10 13:41:16 -0700

    add back arrow functionality; css placeholders

--- assets/js/brainProgramming.js
+++ assets/js/brainProgramming.js
@@ -1,13 +1,37 @@
 var breatheIcon = document.querySelector("#breatheIcon");
 var thoughtsIcon = document.querySelector("#thoughtsIcon");
+var breatheCircle = document.querySelector("#breatheCircle");
+var thoughtsDropDown = document.querySelector("select");
+var backArrow = document.querySelector("#backArrow");
+var breatheSound = document.querySelector("#breatheSound");
 var icons = document.querySelectorAll(".icon");
 
 // hide icons when clicked
 breatheIcon.addEventListener("click", function(){
 	hideIcons();
+	breatheCircle.classList.remove("hidden");
+	backArrow.classList.remove("arrowHidden");
+
+	// FIX--ISN'T PlAYING
+	breatheSound.play();
 });
 thoughtsIcon.addEventListener("click", function(){
 	hideIcons();
+	thoughtsDropDown.classList.remove("hidden");
+	backArrow.classList.remove("arrowHidden");
+});
+
+// clicking back arrow resets hidden classes to inital values, returning to landing screen
+backArrow.addEventListener("click", function(){
+	if(!backArrow.classList.contains("hidden")){
+		for(var i=0; i<icons.length; i++){
+			icons[i].classList.remove("hidden");
+		}
+
+		backArrow.classList.add("arrowHidden");
+		breatheCircle.classList.add("hidden");
+		thoughtsDropDown.classList.add("hidden");
+	}
 });
 
 
