440b537803af567feb411c03346d58d2f537a6da
Jackie Stonebraker <sunktanka@gmail.com>
2017-01-24 10:41:34 -0700

    add breathing circle function and sound

--- assets/js/brainProgramming.js
+++ assets/js/brainProgramming.js
@@ -1,19 +1,19 @@
 var breatheIcon = document.querySelector("#breatheIcon");
 var thoughtsIcon = document.querySelector("#thoughtsIcon");
 var breatheCircle = document.querySelector("#breatheCircle");
-var thoughtsDropDown = document.querySelector("select");
+var thoughtsDropDown = document.querySelector("#thoughtsDropDown");
 var backArrow = document.querySelector("#backArrow");
 var breatheSound = document.querySelector("#breatheSound");
 var icons = document.querySelectorAll(".icon");
+var interval;
 
-// hide icons when clicked
+// icon functionality
 breatheIcon.addEventListener("click", function(){
 	hideIcons();
 	breatheCircle.classList.remove("hidden");
-	backArrow.classList.remove("arrowHidden");
-
-	// FIX--ISN'T PlAYING
 	breatheSound.play();
+	breathing();
+	backArrow.classList.remove("arrowHidden");
 });
 thoughtsIcon.addEventListener("click", function(){
 	hideIcons();
@@ -23,14 +23,23 @@ thoughtsIcon.addEventListener("click", function(){
 
 // clicking back arrow resets hidden classes to inital values, returning to landing screen
 backArrow.addEventListener("click", function(){
+	// remove .hidden from icons
 	if(!backArrow.classList.contains("hidden")){
 		for(var i=0; i<icons.length; i++){
 			icons[i].classList.remove("hidden");
 		}
 
+		// hide other divs
 		backArrow.classList.add("arrowHidden");
 		breatheCircle.classList.add("hidden");
 		thoughtsDropDown.classList.add("hidden");
+
+		// reset breatheCircle
+		breatheCircle.classList.add("inBreath");
+		breatheCircle.classList.remove("outBreath");
+		clearInterval(interval);
+		breatheSound.pause();
+		breatheSound.currentTime = 0;
 	}
 });
 
@@ -40,4 +49,16 @@ function hideIcons(){
 	for(var i=0; i<icons.length; i++){
 		icons[i].classList.add("hidden");
 	}
+}
+
+// TIMES BREATHING ANIMATION
+function breathing(){
+	setTimeout(breath, 10);
+	interval = setInterval(breath, 8000);
+}
+
+// TOGGLES BREATH STATES
+function breath(){
+	breatheCircle.classList.toggle("inBreath");
+	breatheCircle.classList.toggle("outBreath");
 }
